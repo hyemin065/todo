71757878cde4f1e59e5d7292240f4865d2e7323e
hyemin065 <p62517@gmail.com>
2023-11-24 14:56:56 +0900

    fix: join submit함수 조건 수정

--- src/components/organisms/JoinForm.tsx
+++ src/components/organisms/JoinForm.tsx
@@ -2,13 +2,11 @@ import { ChangeEvent } from 'react';
 import styled from 'styled-components';
 import InputGroup from '../molecules/InputGroup';
 import ErrorMessage from '../atoms/ErrorMessage';
-import { JoinErrorMessageType, JoinType } from '../../type/type';
 import Button from '../atoms/Button';
+import { JoinErrorMessageType, JoinType } from '../../type/type';
 
 const JoinErrorMessage = styled(ErrorMessage)`
-  color: red;
   margin-bottom: 20px;
-  font-size: 12px;
 `;
 
 type Props = {
@@ -23,49 +21,61 @@ const JoinForm = ({ data, errorMsg, submitHandler, inputChangeHandler, isLoading
   return (
     <form onSubmit={submitHandler}>
       <InputGroup
-        id='id'
-        label='id'
+        id="id"
+        label="id"
         value={data.id}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => inputChangeHandler('id', e.currentTarget.value)}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          inputChangeHandler('id', e.currentTarget.value)
+        }
         errorMessage={errorMsg.id}
       />
 
       <InputGroup
-        id='password'
-        label='password'
-        type='password'
+        id="password"
+        label="password"
+        type="password"
         value={data.password}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => inputChangeHandler('password', e.currentTarget.value)}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          inputChangeHandler('password', e.currentTarget.value)
+        }
         errorMessage={errorMsg.password}
       />
 
       <InputGroup
-        id='passwordConfirm'
-        label='password confirm'
-        type='password'
+        id="passwordConfirm"
+        label="password confirm"
+        type="password"
         value={data.passwordConfirm}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => inputChangeHandler('passwordConfirm', e.currentTarget.value)}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          inputChangeHandler('passwordConfirm', e.currentTarget.value)
+        }
         errorMessage={errorMsg.passwordConfirm}
       />
 
       <InputGroup
-        id='email'
-        label='email'
+        id="email"
+        label="email"
         value={data.email}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => inputChangeHandler('email', e.currentTarget.value)}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          inputChangeHandler('email', e.currentTarget.value)
+        }
         errorMessage={errorMsg.email}
       />
 
       <InputGroup
-        id='name'
-        label='name'
+        id="name"
+        label="name"
         value={data.name}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => inputChangeHandler('name', e.currentTarget.value)}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          inputChangeHandler('name', e.currentTarget.value)
+        }
         errorMessage={errorMsg.name}
       />
 
-      <JoinErrorMessage errorMsg={errorMsg.error}></JoinErrorMessage>
-      <Button isLoading={isLoading}>회원가입</Button>
+      {errorMsg.error !== '' && <JoinErrorMessage errorMsg={errorMsg.error} />}
+      <Button className="success" isLoading={isLoading}>
+        회원가입
+      </Button>
     </form>
   );
 };
